be20093337f24d7e32f8e0dc49a86d33a480ed8d
Chathan Driehuys <cdriehuys@gmail.com>
2017-01-20 02:16:50 -0500

    Add ex4.

---
+++ lab2/ex4.c
@@ -0,0 +1,22 @@
+/*
+ * Program the displays a formatted table of 6 inputted integers.
+ */
+
+#include <stdio.h>
+
+main() {
+    printf("Enter six integers:\n");
+
+    int nums[6];
+    for (int i = 0; i < 6; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    printf("1234567890bb1234567890\n");
+
+    for (int i = 0; i < 6; i += 2) {
+        printf("%10i  %10i\n", nums[i], nums[i + 1]);
+    }
+
+    return;
+}
